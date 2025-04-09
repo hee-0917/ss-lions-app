dcbb3016c1220a400d16494eaf85d5181504d7c4
junghee <1982nymph@naver.com>
2025-04-10 07:15:20 +0900

    폴더 구조 정리 후 커밋

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider, createTheme } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-  },
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
